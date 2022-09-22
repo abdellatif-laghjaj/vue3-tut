e8a6e6fbd7895c5aea7cbd83b940441c82f2c659
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-22 20:50:53 +0100

    multiple choices

--- src/components/SingUpForm.vue
+++ src/components/SingUpForm.vue
@@ -4,21 +4,12 @@
       <div class="card-body">
         <h1>Registration Form</h1>
         <div class="form-control">
-          <label class="label">
-            <span class="label-text">Email</span>
-          </label>
           <input type="email" placeholder="email" class="input input-bordered" v-model="email" required/>
         </div>
         <div class="form-control">
-          <label class="label">
-            <span class="label-text">Password</span>
-          </label>
           <input type="password" placeholder="password" class="input input-bordered" v-model="password" required/>
         </div>
         <div class="form-control">
-          <label class="label">
-            <span class="label-text">Gender</span>
-          </label>
           <select class="select select-bordered w-full max-w-xs" v-model="gender">
             <option disabled selected>Select gender</option>
             <option value="male">Male</option>
@@ -40,8 +31,6 @@
       </div>
     </div>
     <div class="alert alert-warning shadow-lg flex flex-col">
-      <span v-if="remember_me">I'll remember you</span>
-      <span v-else>I'll forget you</span>
     </div>
   </form>
 </template>
