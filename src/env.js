f67cf0a7b131b0e81747d7062ee901f64e037065
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-24 15:11:07 +0100

    async

---
+++ src/env.js
@@ -0,0 +1,2 @@
+let API_URL = 'http://localhost:3000';
+export default API_URL;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <h1>Home page</h1>
+  <div v-if="error" class="alert alert-error shadow-lg">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+           viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+      </svg>
+      <span>{{ error }}</span>
+    </div>
+  </div>
   <div class="form-control my-3">
     <label class="label cursor-pointer">
       <input type="checkbox" class="toggle toggle-primary" v-model="checked"/>
@@ -12,13 +22,27 @@
 <script setup>
 import {onMounted, onUnmounted, onUpdated, ref, watch} from "vue";
 import PostsList from "@/components/PostsList";
+import API_URL from "@/env";
 
 const checked = ref(true);
-const posts = ref([
-  {id: 1, title: "Web developer", body: "A web developer for our company"},
-  {id: 2, title: "Mobile developer", body: "A mobile developer for our company"},
-  {id: 3, title: "Fullstack developer", body: "A fullstack developer for our company"},
-]);
+const posts = ref([]);
+const error = ref('');
+
+const load = async () => {
+  try {
+    let data = await fetch(API_URL + '/hello')
+    if (!data.ok) {
+      throw Error("Could not fetch the data for that resource");
+    }
+
+    posts.value = await data.json();
+  } catch (err) {
+    error.value = err.message;
+    console.log(err.message)
+  }
+}
+
+load()
 </script>
 
 <style scoped>
