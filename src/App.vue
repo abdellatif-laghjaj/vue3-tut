c0400fb0369bf024b67383c29e654bdc9e5547aa
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-24 01:12:43 +0100

    refs

--- src/App.vue
+++ src/App.vue
@@ -17,20 +17,27 @@
     </div>
     <main class="w-4/5 mx-auto">
       <router-view/>
-      <h2>{{ name }}</h2>
+      <h2 class="font-bold text-2xl mt-5">{{ name }}</h2>
       <button class="my-3 btn gap-2" @click="click">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+        </svg>
         Button
       </button>
+      <input type="text" placeholder="Type here" class="my-3 input input-bordered w-full max-w-xs" v-model="name"/>
     </main>
   </div>
 </template>
 
 <script setup>
-let name = 'Test App'
+import {ref} from "vue";
+
+let name = ref('Test Test')
 
 const click = () => {
-  alert("Hello")
+  name.value = "After Click"
+  console.log(name)
 }
 </script>
 
